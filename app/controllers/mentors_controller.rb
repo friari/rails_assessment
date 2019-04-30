0b46ae8a767c0d50f76d7412e018eeaaa3875b28
Ari <afriedgut1@gmail.com>
2019-05-01 09:45:41 +1000

    created routes and controller

---
+++ app/controllers/mentors_controller.rb
@@ -0,0 +1,49 @@
+class MentorsController < ApplicationController
+    def home
+
+    end
+
+    def index
+
+    end
+
+    def create
+
+    end
+
+    def new
+
+    end
+
+    def edit
+
+    end
+
+    def show
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+    def book
+
+    end
+
+    def review
+
+    end
+
+    def reviews
+
+    end
+
+    def createreview
+
+    end
+end
