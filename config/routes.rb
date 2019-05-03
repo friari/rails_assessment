b082ac939200be1dc9299b0a998a3a48808b28a5
Ari <afriedgut1@gmail.com>
2019-05-03 12:54:42 +1000

    created review form and reviews view

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   get "/", to: "mentors#home", as: "root"
   resources :mentors
   get "/mentors/:id/book", to: "mentors#book", as: "book"
-  get "/mentors/:id/review", to: "mentors#review"
-  get "/mentors/:id/reviews", to: "mentors#reviews"
+  get "/mentors/:id/review", to: "mentors#review", as: "review"
+  get "/mentors/:id/reviews", to: "mentors#reviews", as: "reviews"
   post "/mentors/:id/reviews", to: "mentors#createreview"
   get "/users/:id/profile", to: "mentors#profile", as: "profile"
 end
