1d36811aaab50530fa2e66a9c6631c03fcb261c3
sjwconway <sjwconway@hotmail.com>
2019-05-07 12:34:48 +1000

    Added My Bookings to the Profile page view

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,7 @@
 class ProfilesController < ApplicationController
     def index
-            
+        @bookings = User.find(current_user.id).bookings 
+        @user_booking = User.find(current_user.id).bookings
     end
     
 end
