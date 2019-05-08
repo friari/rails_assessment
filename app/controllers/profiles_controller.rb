584984adca1548845013bfe14446893f839845a1
sjwconway <sjwconway@hotmail.com>
2019-05-08 14:14:03 +1000

    fixed merge

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,7 +3,7 @@ class ProfilesController < ApplicationController
         @user_booking = User.find(current_user.id).bookings
         @mentor_details = User.find(current_user.id).mentor
         @mentor_clients = User.find(current_user.id).bookings
-        @check_bookings = @user_booking.each {|book| book.id}
+        
         
         # @booking_date = User.find(current_user.id).bookings.created_at
     end
