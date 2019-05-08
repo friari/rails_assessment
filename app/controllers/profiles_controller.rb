eaa553f27b8ab50b30387c746d6ecde988a6e858
sjwconway <sjwconway@hotmail.com>
2019-05-08 14:10:53 +1000

    undid changes

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,6 +3,8 @@ class ProfilesController < ApplicationController
         @user_booking = User.find(current_user.id).bookings
         @mentor_details = User.find(current_user.id).mentor
         @mentor_clients = User.find(current_user.id).bookings
+        @check_bookings = @user_booking.each {|book| book.id}
+        
         # @booking_date = User.find(current_user.id).bookings.created_at
     end
     
